a710c050e274ca824edfcb763f27bbd6b0f1651d
Kani <kahnyee2005@gmail.com>
2023-06-17 20:34:02 +0800

    BMI Test Case

--- BMI/bmi.py
+++ BMI/bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height, weight):
 
     if BMI < 18.5 :
         BMI_Class = "Under Weight"
+        return -1
     elif 18.5 <= BMI <= 25.0 :
         BMI_Class = "Normal"
+        return 0
     else :
         BMI_Class = "Over Weight"
+        return 1
 
     print("BMI = " + str(BMI))
     print("You are : " + BMI_Class)
